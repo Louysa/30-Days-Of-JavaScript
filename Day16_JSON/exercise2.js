6c89a80276dd474b3853b0bf5158e17091dd6efc
Doğan Can Oz <doganlouysa@outlook.com>
2024-03-12 13:02:43 +0300

    Day16

---
+++ Day16_JSON/exercise2.js
@@ -0,0 +1,10 @@
+const student = {
+    firstName:'Asabeneh',
+    lastName:'Yetayehe',
+    age:250,
+    isMarried:true,
+    skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+  }
+
+const studentObject = JSON.stringify(student,['firstName','lastName','skills'],4);
+console.log(studentObject);
